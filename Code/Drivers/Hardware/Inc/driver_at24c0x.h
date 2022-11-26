24db2707de73d21ccec1d83d3f31f750b4a6ca01
Harry-Qu <878964200@qq.com>
2022-11-26 23:30:37 +0800

    新增从指定页中读写数据的接口driver_at24c0x_writeData_page, driver_at24c0x_readData_page 修改部分接口返回类型为uint8_t

--- Code/Drivers/Hardware/Inc/driver_at24c0x.h
+++ Code/Drivers/Hardware/Inc/driver_at24c0x.h
@@ -1,10 +1,13 @@
 /** 
  *  @brief	    AT24C0x驱动
- *  @details    支持读取固定某页的(256*8K)的不限类型不限长度的数据
+ *  @details    支持从AT24C0X指定页读写的不限类型不限长度的数据
  *  @author     Harry-Qu
  *  @date       2022/11/5
- *  @version    1.0
+ *  @version    1.1
  *  @par        日志
+ *              1.0     |   实现AT24C0x基本读写功能
+ *              1.1     |   新增从指定页中读写数据的接口driver_at24c0x_writeData_page, driver_at24c0x_readData_page
+ *                          修改部分接口返回类型为uint8_t
 */
 
 #ifndef DRIVER_AT24C0X_H
@@ -13,18 +16,10 @@
 #include "main.h"
 #include "i2c.h"
 
-#define AT24C0X_I2C hi2c1
+#define AT24C0X_I2C hi2c3
 #define AT24C0X_READ_ADDRESS 0xA1
 #define AT24C0X_WRITE_ADDRESS 0xA0
 
-/**
- * 从AT24C0X中读取数据
- * @param address 数据地址
- * @param data 读出的数据指针，单数据/数组
- * @param dataSize 数据大小，单数据大小*数据长度
- * @return 读取状态
- */
-HAL_StatusTypeDef driver_at24c0x_readData(uint8_t address, void *data, uint32_t dataSize);
 
 /**
  * 向AT24C0X中写入数据
@@ -32,7 +27,44 @@ HAL_StatusTypeDef driver_at24c0x_readData(uint8_t address, void *data, uint32_t 
  * @param data 写入的数据指针，单数据/数组
  * @param dataSize 数据大小，单数据大小*数据长度
  * @return 写入状态
+ *         0: success
+ *         1: fail
  */
-HAL_StatusTypeDef driver_at24c0x_writeData(uint8_t address, void *data, uint32_t dataSize);
+uint8_t driver_at24c0x_writeData(uint8_t address, void *data, uint32_t dataSize);
+
+/**
+ * 从AT24C0X中读取数据
+ * @param address 数据地址
+ * @param data 读出的数据指针，单数据/数组
+ * @param dataSize 数据大小，单数据大小*数据长度
+ * @return 读取状态
+ *         0: success
+ *         1: fail
+ */
+uint8_t driver_at24c0x_readData(uint8_t address, void *data, uint32_t dataSize);
+
+/**
+ * 向AT24C0X指定页中写入数据
+ * @param page 要写入的ROM分页
+ * @param address 要写入的ROM开始地址
+ * @param data 写入的数据指针，单数据/数组
+ * @param dataSize 数据大小，单数据大小*数据长度
+ * @return 写入状态
+ *         0: success
+ *         1: fail
+ */
+uint8_t driver_at24c0x_writeData_page(uint8_t page, uint8_t address, void *data, uint32_t dataSize);
+
+/**
+ * 从AT24C0X指定页中读取数据
+ * @param page 要读取的ROM分页
+ * @param address 要读取的ROM开始地址
+ * @param data 读出的数据指针，单数据/数组
+ * @param dataSize 数据大小，单数据大小*数据长度
+ * @return 写入状态
+ *         0: success
+ *         1: fail
+ */
+uint8_t driver_at24c0x_readData_page(uint8_t page, uint8_t address, void *data, uint32_t dataSize);
 
 #endif //DRIVER_AT24C0X_H
--- Code/Drivers/Hardware/Src/driver_at24c0x.c
+++ Code/Drivers/Hardware/Src/driver_at24c0x.c
@@ -1,15 +1,15 @@
 /** 
  *  @brief	    AT24C0x驱动
- *  @details    支持读取固定某页的(256*8K)的不限类型不限长度的数据
+ *  @details    支持从AT24C0X指定页读写的不限类型不限长度的数据
  *  @author     Harry-Qu
  *  @date       2022/11/5
- *  @version    1.0
+ *  @version    1.1
  *  @par        日志
-*/
-
-
-/**
- * 代码示例:
+ *              1.0     |   实现AT24C0x基本读写功能
+ *              1.1     |   新增从指定页中读写数据的接口driver_at24c0x_writeData_page, driver_at24c0x_readData_page
+ *                          修改部分接口返回类型为uint8_t
+ *
+ * @code 代码示例:
  * {
  *      uint8_t data = 0x18;
  *      driver_at24c0x_writeData(0x00, &data, sizeof(data));    //向0x00地址写入1字节数据0x18
@@ -22,18 +22,18 @@
  *
  * {
  *      float data[3] = {-1.0f, 2.5f, 3.14f};
- *      driver_at24c0x_writeData(0x00, &data, sizeof(data));    //向0x00地址写入3个float数据，共12字节
+ *      driver_at24c0x_writeData_page(1, 0x00, &data, sizeof(data));    //向0x00地址写入3个float数据，共12字节
  * }
  *
  * {
  *      float data[3];
- *      driver_at24c0x_readData(0x00, &data, sizeof(data));    //从0x00地址读取3个float数据，共12字节
+ *      driver_at24c0x_readData_page(1, 0x00, &data, sizeof(data));    //从0x00地址读取3个float数据，共12字节
  * }
  */
 
 #include "driver_at24c0x.h"
 
-uint32_t at24c0x_LastVisitTick = 0;
+static uint32_t at24c0x_LastVisitTick = 0;
 
 /**
  * 读写之间需间隔5ms
@@ -59,15 +59,23 @@ static void driver_at24c0x_RecordVisitTime(void) {
  * @param Size 数据大小(字节)
  * @param Timeout 超时时间
  * @return 写入状态
+ *         0: success
+ *         1: fail
  */
-static HAL_StatusTypeDef
-driver_at24c0x_memWrite(uint8_t address, uint8_t *pData, uint16_t Size, uint32_t Timeout) {
-    driver_at24c0x_checkDeltaTime();
+static uint8_t
+driver_at24c0x_memWrite(uint8_t page, uint8_t address, uint8_t *pData, uint16_t Size, uint32_t Timeout) {
     HAL_StatusTypeDef status;
-    status = HAL_I2C_Mem_Write(&AT24C0X_I2C, AT24C0X_WRITE_ADDRESS, address, I2C_MEMADD_SIZE_8BIT, pData, Size,
+    uint8_t devAddress = AT24C0X_WRITE_ADDRESS | (page << 1);
+
+    driver_at24c0x_checkDeltaTime();
+
+    status = HAL_I2C_Mem_Write(&AT24C0X_I2C, devAddress, address, I2C_MEMADD_SIZE_8BIT, pData, Size,
                                Timeout);
     driver_at24c0x_RecordVisitTime();
-    return status;
+    if (status > 0) {
+        return 1;
+    }
+    return 0;
 }
 
 /**
@@ -77,24 +85,44 @@ driver_at24c0x_memWrite(uint8_t address, uint8_t *pData, uint16_t Size, uint32_t
  * @param Size 数据大小(字节)
  * @param Timeout 超时时间
  * @return 读取状态
+ *         0: success
+ *         1: fail
  */
-static HAL_StatusTypeDef
-driver_at24c0x_memRead(uint8_t address, uint8_t *pData, uint16_t Size, uint32_t Timeout) {
+static uint8_t
+driver_at24c0x_memRead(uint8_t page, uint8_t address, uint8_t *pData, uint16_t Size, uint32_t Timeout) {
+    HAL_StatusTypeDef status;
+    uint8_t devAddress = AT24C0X_WRITE_ADDRESS | (page << 1);
+
     driver_at24c0x_checkDeltaTime();
-    HAL_StatusTypeDef status;
-    status = HAL_I2C_Mem_Read(&AT24C0X_I2C, AT24C0X_WRITE_ADDRESS, address, I2C_MEMADD_SIZE_8BIT, pData, Size, Timeout);
+
+    status = HAL_I2C_Mem_Read(&AT24C0X_I2C, devAddress, address, I2C_MEMADD_SIZE_8BIT, pData, Size, Timeout);
     driver_at24c0x_RecordVisitTime();
+    if (status > 0) {
+        return 1;
+    }
+    return 0;
+}
+
+uint8_t driver_at24c0x_writeData(uint8_t address, void *data, uint32_t dataSize) {
+    HAL_StatusTypeDef status;
+    status = driver_at24c0x_memWrite(0, address, data, dataSize, 10);
     return status;
 }
 
-HAL_StatusTypeDef driver_at24c0x_writeData(uint8_t address, void *data, uint32_t dataSize) {
+uint8_t driver_at24c0x_readData(uint8_t address, void *data, uint32_t dataSize) {
     HAL_StatusTypeDef status;
-    status = driver_at24c0x_memWrite(address, data, dataSize, 10);
+    status = driver_at24c0x_memRead(0, address, data, dataSize, 10);
     return status;
 }
 
-HAL_StatusTypeDef driver_at24c0x_readData(uint8_t address, void *data, uint32_t dataSize) {
+uint8_t driver_at24c0x_writeData_page(uint8_t page, uint8_t address, void *data, uint32_t dataSize) {
     HAL_StatusTypeDef status;
-    status = driver_at24c0x_memRead(address, data, dataSize, 10);
+    status = driver_at24c0x_memWrite(page, address, data, dataSize, 10);
+    return status;
+}
+
+uint8_t driver_at24c0x_readData_page(uint8_t page, uint8_t address, void *data, uint32_t dataSize) {
+    HAL_StatusTypeDef status;
+    status = driver_at24c0x_memRead(page, address, data, dataSize, 10);
     return status;
 }
