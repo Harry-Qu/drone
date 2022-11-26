0fe6e1532dd69c48ae7427da797e6cbb496ef07e
Harry-Qu <878964200@qq.com>
2022-11-26 22:39:32 +0800

    新增向I2C设备指定内存地址写入数据功能

--- Code/SDK/Inc/sdk_i2c.h
+++ Code/SDK/Inc/sdk_i2c.h
@@ -3,11 +3,13 @@
  *  @details    普通,DMA方式发送,接收数据
  *  @author     Harry-Qu
  *  @date       2022/5/18
- *  @version    1.2
+ *  @version    1.2.1
  *  @par        日志
  *              1.0     |       实现普通和DMA方式发送接收数据功能。
  *              1.1     |       新增从指定内存地址读取数据功能。
  *              1.2     |       降低与ucos-II的耦合，可在非操作系统环境下使用
+ *              1.2.1   |       新增向I2C设备指定内存地址写入数据功能
+ *                              新增接口为sdk_i2c_memory_write, sdk_i2c_memory_write_dma
 */
 
 #ifndef SDK_I2C_H
@@ -88,7 +90,7 @@ HAL_StatusTypeDef
 sdk_i2c_receive_dma(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout,
                     uint8_t *perr);
 /**
- * 从指定设备指定地址中读取数据信息
+ * 从指定设备指定地址中读取数据信息（阻塞方式）
  * @param hi2c i2c句柄
  * @param DevAddress 设备地址
  * @param MemAddress 待读取的寄存器地址
@@ -103,7 +105,7 @@ sdk_i2c_memory_read(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAd
                     uint8_t *pData, uint16_t Size, uint32_t Timeout);
 
 /**
- * 从指定设备指定地址中读取数据信息
+ * 从指定设备指定地址中读取数据信息（DMA方式）
  * @param hi2c i2c句柄
  * @param DevAddress 设备地址
  * @param MemAddress 待读取的寄存器地址
@@ -118,5 +120,36 @@ HAL_StatusTypeDef
 sdk_i2c_memory_read_dma(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize,
                         uint8_t *pData, uint16_t Size, uint32_t Timeout,
                         uint8_t *perr);
+
+/**
+ * 向指定设备指定地址中写入数据信息（阻塞方式）
+ * @param hi2c i2c句柄
+ * @param DevAddress 设备地址
+ * @param MemAddress 待读取的寄存器地址
+ * @param MemAddSize 待读取的寄存器大小（一般是I2C_MEMADD_SIZE_8BIT，即1）
+ * @param pData 数据地址
+ * @param Size 数据大小
+ * @param Timeout 超时时间
+ * @return
+ */
+HAL_StatusTypeDef
+sdk_i2c_memory_write(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize,
+                     uint8_t *pData, uint16_t Size, uint32_t Timeout);
+
+/**
+ * 向指定设备指定地址中写入数据信息（DMA方式）
+ * @param hi2c i2c句柄
+ * @param DevAddress 设备地址
+ * @param MemAddress 待读取的寄存器地址
+ * @param MemAddSize 待读取的寄存器大小（一般是I2C_MEMADD_SIZE_8BIT，即1）
+ * @param pData 数据地址
+ * @param Size 数据大小
+ * @param Timeout 超时时间
+ * @param perr 错误信息
+ * @return
+ */
+HAL_StatusTypeDef
+sdk_i2c_memory_write_dma(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize,
+                         uint8_t *pData, uint16_t Size, uint32_t Timeout, uint8_t *perr);
 
 #endif //SDK_I2C_H
--- Code/SDK/Src/sdk_i2c.c
+++ Code/SDK/Src/sdk_i2c.c
@@ -3,11 +3,13 @@
  *  @details    普通,DMA方式发送,接收数据
  *  @author     Harry-Qu
  *  @date       2022/5/18
- *  @version    1.2
+ *  @version    1.2.1
  *  @par        日志
  *              1.0     |       实现普通和DMA方式发送接收数据功能。
  *              1.1     |       新增从指定内存地址读取数据功能。
  *              1.2     |       降低与ucos-II的耦合，可在非操作系统环境下使用
+ *              1.2.1   |       新增向I2C设备指定内存地址写入数据功能
+ *                              新增接口为sdk_i2c_memory_write, sdk_i2c_memory_write_dma
 */
 
 #include "sdk_i2c.h"
@@ -344,4 +346,91 @@ sdk_i2c_memory_read_dma(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t M
 
 #endif  //OS_uCOS_II_H
 }
+
+HAL_StatusTypeDef
+sdk_i2c_memory_write(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize,
+                     uint8_t *pData, uint16_t Size, uint32_t Timeout) {
+    HAL_StatusTypeDef status;
+
+#ifdef OS_uCOS_II_H
+    if (OSRunning == OS_TRUE) {
+        OSSchedLock();
+        status = HAL_I2C_Mem_Write(hi2c, DevAddress, MemAddress, MemAddSize, pData, Size, Timeout);
+        OSSchedUnlock();
+    } else {
+        status = HAL_I2C_Mem_Write(hi2c, DevAddress, MemAddress, MemAddSize, pData, Size, Timeout);
+    }
+#else
+    status = HAL_I2C_Mem_Write(hi2c, DevAddress, MemAddress, MemAddSize, pData, Size, Timeout);
+#endif
+
+    return status;
+}
+
+HAL_StatusTypeDef
+sdk_i2c_memory_write_dma(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize,
+                         uint8_t *pData, uint16_t Size, uint32_t Timeout, uint8_t *perr) {
+#ifdef OS_uCOS_II_H
+
+
+#if I2C_DMA_EN == 0
+    *perr = 0;
+    return sdk_i2c_memory_write(hi2c, DevAddress, MemAddress, MemAddSize, pData, Size, Timeout > 0 ? Timeout : 100);
+#else
+
+    if (OSRunning == OS_FALSE || hi2c->hdmarx == NULL) {
+        //当前没在跑操作系统或接收没开DMA，不能调用DMA功能
+        *perr = 0;
+        return sdk_i2c_memory_write(hi2c, DevAddress, MemAddress, MemAddSize, pData, Size, Timeout > 0 ? Timeout : 100);
+    }
+
+    Timeout = 0;
+
+#if MULTITASK_OCCUPIES_THE_SAME_I2C > 0
+    OS_EVENT *hi2cFreeSem = Get_I2C_Sem(i2cFreeSem, hi2c);
+    OSSemPend(hi2cFreeSem, Timeout, perr);
+#endif
+
+
+    OS_EVENT *hi2cCompleteSem = Get_I2C_Sem(i2cCompletedSem, hi2c);
+    OSSemSet(hi2cCompleteSem, 0, perr);
+
+
+//    I2C状态异常报error
+//    if (hi2c->State != HAL_I2C_STATE_READY || hi2c->hdmatx->State != HAL_DMA_STATE_READY) {
+//        SEGGER_SYSVIEW_Error("not ready");
+//    }
+
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Write_DMA(hi2c, DevAddress, MemAddress, MemAddSize, pData, Size);
+    //运行结束会调用HAL_I2C_MemRxCpltCallback
+
+    if (status != HAL_OK) {
+        *perr = 0;
+#if MULTITASK_OCCUPIES_THE_SAME_I2C > 0
+        OSSemPost(hi2cFreeSem);
+#endif
+        return status;
+    }
+
+    OSSemPend(hi2cCompleteSem, Timeout, perr);
+    if (*perr != OS_ERR_NONE) {
+    }
+
+#if MULTITASK_OCCUPIES_THE_SAME_I2C > 0
+    OSSemPost(hi2cFreeSem);
+#endif
+
+    return HAL_OK;
+
+#endif  //I2C_DMA_EN
+
+#else   //OS_uCOS_II_H
+
+    *perr = 0;
+    return sdk_i2c_memory_write(hi2c, DevAddress, MemAddress, MemAddSize, pData, Size, Timeout > 0 ? Timeout : 100);
+
+#endif  //OS_uCOS_II_H
+}
+
+
 
